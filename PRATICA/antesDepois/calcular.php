38ba12e18d290e42d0bd03b951b5810c14a93251
victorviegasgomes <vviegasgomes@gmail.com>
2025-03-30 19:54:46 +0100

    criando saites com lógica php

---
+++ PRATICA/antesDepois/calcular.php
@@ -0,0 +1,17 @@
+<?php
+$numero = $_POST['numero'];
+
+echo "o sucessor de $numero é: ".($numero+1)."<br>";
+print "o antecessor de $numero é: ".($numero-1);
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<style>
+* {
+								font-family: arial, helvetica, sans-serif;
+}
+</style>
+</meta>
+</html>
---
+++ PRATICA/antesDepois/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+								<meta charset="utf-8">
+								<meta name="viewport" content="width=device-width,initial-scale=1.0">
+								<meta http-equiv="CONTENT-TYPE" content="text/html; charset=UTF-8">
+								<link rel="stylesheet" href="style/style.css">
+								<title>Hello, World!</title>
+</head>
+<body>
+								<div id="conteiner">
+								<h1>antesseror e sucessor</h1>
+																<form action="calcular.php" method="post">
+																								<label for="numero">informe um número</label>
+																								<input type="number" name="numero" id="numero" placeholder="digite um número">
+																								<button>verificar</button>
+																</form>
+	   </div>																							
+</body>
+</html>
+
+

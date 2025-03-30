7d7381c2f7db5d181eece23b0f3a885379400157
victorviegasgomes <vviegasgomes@gmail.com>
2025-03-30 01:20:16 +0100

    começando a estudar serio

---
+++ PRATICA/formulario01/index.php
@@ -0,0 +1,27 @@
+<DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<title>página inicial</title>
+	</head>
+	<body>
+		<header>
+			<h1>Apresente-se</h1>
+		</header>
+		<section>
+			<form method="GET" action="registrar.php">
+				<p>
+				<label for="idnome">nome</label><br>
+				<input type="text" name="nome" id="idnome">
+				</p>
+				<p>
+				<label for="idsobrenome">sobrenome</label><br>
+				<input type="text" name="sobrenome" id="idsobrenome">
+				</p>
+				<p>
+				<input type="submit" value="enviar">
+			</form>
+		</section>
+	</body>
+</html>
---
+++ PRATICA/formulario01/registrar.php
@@ -0,0 +1,11 @@
+<?php
+$nome = $_GET["nome"];
+$sobre = $_GET["sobrenome"];
+echo "Olá, <strong>$nome $sobre</strong> seja bem vindo ao php";
+?>
+<DOCTYOE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+</html>
